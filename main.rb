e7d67bd149d9fb53c2fe46bd1003e872e7b8feef
Marji Symonds <marji712@gmail.com>
2021-05-09 13:59:33 -0700

    Introduce Parameter Object - Move object creation to before function call

--- main.rb
+++ main.rb
@@ -1,21 +1,24 @@
-def draw_button(label_text, x, y, foreground_color, is_dark_mode)
-  button = Button.new(label_text, x, y, foreground_color)
-  if is_dark_mode
+def draw_button(button)
+  if button.is_dark_mode
     # darken foreground color for dark mode
-    paint(label_text, x, y, foreground_color - 10, '#111111')
+    paint(button.label_text, button.x, button.y, button.foreground_color - 10, '#111111')
   else
     # lighten foreground color for non-dark mode
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+    paint(button.label_text, button.x, button.y, button.foreground_color + 10, '#E0E0E0')
   end
 end
 
 class Button
-  attr_accessor :label_text, :x, :y, :foreground_color
+  attr_accessor :label_text, :x, :y, :foreground_color, :is_dark_mode
 
   def initialize(label_text, x, y, foreground_color)
     @label_text = label_text
     @x = x
     @y = y
     @foreground_color = foreground_color
+    @is_dark_mode = is_dark_mode
   end
 end
+
+button = Button.new(label_text, x, y, foreground_color, is_dark_mode)
+draw_button(button)
