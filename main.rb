5e97bf8d171e07f787d530dbdb506e7bd7569d41
Marji Symonds <marji712@gmail.com>
2021-05-09 14:06:36 -0700

    Introduce Parameter Object - Revert is_dark_mode to param to try Remove Flag Argument

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
-def draw_button(button)
-  if button.is_dark_mode
+def draw_button(button, is_dark_mode)
+  if is_dark_mode
     # darken foreground color for dark mode
     paint(button.label_text, button.x, button.y, button.foreground_color - 10, '#111111')
   else
@@ -9,16 +9,15 @@ def draw_button(button)
 end
 
 class Button
-  attr_accessor :label_text, :x, :y, :foreground_color, :is_dark_mode
+  attr_accessor :label_text, :x, :y, :foreground_color,
 
   def initialize(label_text, x, y, foreground_color)
     @label_text = label_text
     @x = x
     @y = y
     @foreground_color = foreground_color
-    @is_dark_mode = is_dark_mode
   end
 end
 
-button = Button.new(label_text, x, y, foreground_color, is_dark_mode)
+button = Button.new(label_text, x, y, foreground_color)
 draw_button(button)
