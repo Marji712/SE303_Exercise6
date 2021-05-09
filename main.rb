50f75a7e73750364362276801f8451346ac2cf7c
Marji Symonds <marji712@gmail.com>
2021-05-09 14:12:00 -0700

    Remove Flag Argument - use Decompose Conditional to make two functions to better capture intent

--- main.rb
+++ main.rb
@@ -1,11 +1,19 @@
 def draw_button(button, is_dark_mode)
   if is_dark_mode
     # darken foreground color for dark mode
-    paint(button.label_text, button.x, button.y, button.foreground_color - 10, '#111111')
+    draw_dark_mode_button(button)
   else
     # lighten foreground color for non-dark mode
-    paint(button.label_text, button.x, button.y, button.foreground_color + 10, '#E0E0E0')
+    draw_light_mode_button(button)
   end
+end
+
+def draw_dark_mode_button(button)
+  paint(button.label_text, button.x, button.y, button.foreground_color - 10, '#111111')
+end
+
+def draw_light_mode_button(button)
+  paint(button.label_text, button.x, button.y, button.foreground_color + 10, '#E0E0E0')
 end
 
 class Button
